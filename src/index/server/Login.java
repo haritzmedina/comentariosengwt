dc5bfc304411eadb5d9d3350d746d86700562fbf
pirque07@gmail.com <pirque07@gmail.com@1ff1b2e6-f771-7246-3f5b-6d26f325f6b2>
2013-05-09 17:46:10 +0000

    Con pagina de login + registro funcionando parcialmente

---
+++ src/index/server/Login.java
@@ -0,0 +1,42 @@
+package index.server;
+
+import index.client.LoginService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class Login extends RemoteServiceServlet implements
+		LoginService {
+
+	public Integer login(String user, String pass) throws IllegalArgumentException {
+		String str = new String();
+		//Se verifica si el usuario existe
+		if(AccesoBD.crearConexion())
+		{
+			Long consulta = (Long) AccesoBD.selectEscalar("count(*)", "Usuarios", "(Username='"+user+"' || Email='"+user+"') && Password='"+pass+"'");
+			if(consulta!= null && consulta <= 0)
+				return 0;
+			else
+				return 1;
+		}
+		else
+			return -1;
+	}
+
+	/**
+	 * Escape an html string. Escaping data received from the client helps to
+	 * prevent cross-site script vulnerabilities.
+	 * 
+	 * @param html the html string to escape
+	 * @return the escaped string
+	 */
+	private String escapeHtml(String html) {
+		if (html == null) {
+			return null;
+		}
+		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
+				.replaceAll(">", "&gt;");
+	}
+}
